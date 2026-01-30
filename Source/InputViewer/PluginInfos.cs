dd07c8ada26e1370710e139617fd3bc3b2a4b2a5
avgduck <avgduck@gmail.com>
2026-01-29 22:54:17 -0500

    Increment major version

--- Source/InputViewer/PluginInfos.cs
+++ Source/InputViewer/PluginInfos.cs
@@ -32,6 +32,6 @@ namespace InputViewer
         /// Version of the plugin. Must be in form <major>.<minor>.<build>.<revision>.
         /// Major and minor versions are mandatory, but build and revision can be left unspecified.
         /// </summary>
-        public const string PLUGIN_VERSION = "2.1.0";
+        public const string PLUGIN_VERSION = "3.0.0";
     }
 }
